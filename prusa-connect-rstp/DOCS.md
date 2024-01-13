bc08b935e0021ab53ed23ba46c029fc36f309a3b
Botond24 <44529287+Botond24@users.noreply.github.com>
2024-01-13 23:26:36 +0100

    Initial Release

---
+++ prusa-connect-rstp/DOCS.md
@@ -0,0 +1,62 @@
+# Home Assistant Add-on: Prusa Connect RSTP
+
+## Installation
+
+Follow these steps to get the add-on installed on your system:
+
+[![Add repository on my Home Assistant][repository-badge]][repository-url]
+
+1. Click on the above badge.
+2. Navigate in your Home Assistant frontend to **Settings** -> **Add-ons** -> **Add-on store**.
+3. Find the "Prusa Connect RTSP" add-on and click it.
+4. Click on the "INSTALL" button.
+
+## How to use
+
+1. Add `CAMERA_URLS` to your add-on configuration.
+2. Add `TOKENS` to your add-on configuration.
+3. Start the add-on.
+
+## Add-on Configuration
+
+The MariaDB server add-on can be tweaked to your likings. This section
+describes each of the add-on configuration options.
+
+Example add-on configuration:
+
+```yaml
+CAMERA_URLS: ["http://244.178.44.111:8080"]
+TOKENS: ["ahdfblagfzVuzhuh"]
+```
+
+### Option: `CAMERA_URLS` (required)
+
+This section defines the URLs of the cameras. For example, `rstp://244.178.44.111:554/video/channel/1` or `http://244.178.44.111:8080`.
+
+### Option: `TOKENS` (required)
+
+This section defines the tokens of the cameras in the same order as `CAMERA_URLS`.
+
+### Option: `FRAME_CAPTURE_DELAY` (optional)
+
+How many seconds should be waited between each camera's frame capture. Default is `1`.
+
+### Option: `CAMERA_CYCLE_DELAY` (optional)
+
+How many seconds should be waited restarting the cycle. Default is `10`.
+
+### Option: `CONNECTION_TIMEOUT_DELAY` (optional)
+
+How many seconds should be waited for the connection to be established. Default is `5`.
+
+### Option: `log_level` (optional)
+
+This section defines the log level of the add-on.
+
+## Support
+
+In case you've found a bug, please [open an issue on our GitHub][issue].
+
+[issue]: https://github.com/Botond24/prusa_connect_rtsp_homeassistant/issues
+[repository-badge]: https://img.shields.io/badge/Add%20repository%20to%20my-Home%20Assistant-41BDF5?logo=home-assistant&style=for-the-badge
+[repository-url]: https://my.home-assistant.io/redirect/supervisor_add_addon_repository/?repository_url=https://github.com/Botond24/prusa_connect_rtsp_homeassistant/issues
---
+++ prusa-connect-rstp/run.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/with-contenv bashio
+
+trap "echo SIGINT received, exiting...; exit 0" INT
+
+CAMERA_URLS=$(bashio::config 'CAMERA_URLS')
+TOKENS=$(bashio::config 'TOKENS')
+FRAME_CAPTURE_DELAY=$(bashio::config 'FRAME_CAPTURE_DELAY')
+CAMERA_CYCLE_DELAY=$(bashio::config 'CAMERA_CYCLE_DELAY')
+CONNECTION_TIMEOUT_DELAY=$(bashio::config 'CONNECTION_TIMEOUT_DELAY')
+
+
+bashio::log.info "     ██╗████████╗███████╗███████╗██████╗ ██████╗"
+bashio::log.info "     ██║╚══██╔══╝██╔════╝██╔════╝╚════██╗██╔══██╗"
+bashio::log.info "     ██║   ██║   █████╗  █████╗   █████╔╝██║  ██║"
+bashio::log.info "██   ██║   ██║   ██╔══╝  ██╔══╝   ╚═══██╗██║  ██║"
+bashio::log.info "╚█████╔╝   ██║   ███████╗███████╗██████╔╝██████╔╝"
+bashio::log.info " ╚════╝    ╚═╝   ╚══════╝╚══════╝╚═════╝ ╚═════╝ "
+bashio::log.info ""
+bashio::log.info "This script sends snapshots of RTSP and MJPEG streams to Prusa Connect."
+bashio::log.info ""
+: "${PRUSA_URL:=https://webcam.connect.prusa3d.com/c/snapshot}"
+: "${CAMERA_URLS:=}"
+: "${TOKENS:=}"
+
+declare log_level
+
+log_level=$(bashio::string.lower "$(bashio::config log_level invalid)")
+if [ "$log_level" = "invalid" ]; then
+  bashio::log.magenta 'Received invalid log_level from config, fallback to info'
+  log_level="info"
+fi
+bashio::log.level "$log_level"
+
+CAMERA_URLS=$(echo "$CAMERA_URLS" | tr -d ' ')
+TOKENS=$(echo "$TOKENS" | tr -d ' ')
+FRAME_CAPTURE_DELAY=${FRAME_CAPTURE_DELAY:-1}
+CAMERA_CYCLE_DELAY=${CAMERA_CYCLE_DELAY:-10}
+CONNECTION_TIMEOUT_DELAY=${CONNECTION_TIMEOUT_DELAY:-5}
+
+readarray -t TOKENS <<<"$TOKENS"
+readarray -t CAMERA_URLS <<< "$CAMERA_URLS"
+
+FINGERPRINTS=()
+for i in $(seq 1 ${#CAMERA_URLS[@]}); do
+    FINGERPRINTS+=($(printf "camera%010d" $i))
+done
+
+bashio::log.info "Input variables:"
+for i in "${!CAMERA_URLS[@]}"; do
+    bashio::log.info "Camera $((i + 1)), URL: ${CAMERA_URLS[$i]}, ${TOKENS[$i]}"
+done
+
+while true; do
+    for i in "${!CAMERA_URLS[@]}"; do
+        bashio::log.debug "Processing camera: $((i + 1))"
+        bashio::log.debug "URL: ${CAMERA_URLS[$i]}"
+        bashio::log.debug "Token: ${TOKENS[$i]}"
+        bashio::log.debug "Fingerprint: ${FINGERPRINTS[$i]}"
+        bashio::log.debug "------"
+        if [[ ${CAMERA_URLS[$i]} == *"rtsp"* ]]; then
+            ffmpeg \
+                -loglevel error \
+                -y \
+                -rtsp_transport tcp \
+                -i "${CAMERA_URLS[$i]}" \
+                -f image2 \
+                -vframes 1 \
+                -pix_fmt yuvj420p \
+                -timeout "$CONNECTION_TIMEOUT_DELAY" \
+                output_$i.jpg
+        else
+            ffmpeg \
+                -loglevel error \
+                -y \
+                -i "${CAMERA_URLS[$i]}" \
+                -f image2 \
+                -vframes 1 \
+                -pix_fmt yuvj420p \
+                -timeout "$CONNECTION_TIMEOUT_DELAY" \
+                output_$i.jpg
+        fi
+
+        if [ $? -eq 0 ]; then
+            curl -X PUT "$PRUSA_URL" \
+                -H "accept: */*" \
+                -H "content-type: image/jpg" \
+                -H "fingerprint: ${FINGERPRINTS[$i]}" \
+                -H "token: ${TOKENS[$i]}" \
+                --data-binary "@output_$i.jpg" \
+                --no-progress-meter \
+                --compressed \
+                --max-time "$CONNECTION_TIMEOUT_DELAY"
+        else
+            bashio::log.error "FFmpeg returned an error for camera $((i + 1))."
+        fi
+        sleep "$FRAME_CAPTURE_DELAY"
+    done
+
+    sleep "$CAMERA_CYCLE_DELAY"
+done
